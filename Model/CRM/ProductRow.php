cf879b78de348e8d3795c89d1362d4b9894e59b5
Igor Nikolaev <igor.sv.n@gmail.com>
2020-12-18 16:56:52 +0300

    Update product row model.

--- Model/CRM/ProductRow.php
+++ Model/CRM/ProductRow.php
@@ -17,24 +17,99 @@ use Darvin\Bitrix24Bundle\Model\AbstractModel;
  */
 class ProductRow extends AbstractModel
 {
+    /**
+     * @var int|null
+     */
     private $id;
+
+    /**
+     * @var int|null
+     */
     private $ownerId;
+
+    /**
+     * @var string|null
+     */
     private $ownerType;
+
+    /**
+     * @var int
+     */
     private $productId;
+
+    /**
+     * @var string|null
+     */
     private $productName;
+
+    /**
+     * @var float|null
+     */
     private $price;
+
+    /**
+     * @var float|null
+     */
     private $priceExclusive;
+
+    /**
+     * @var float|null
+     */
     private $priceNetto;
+
+    /**
+     * @var float|null
+     */
     private $priceBrutto;
+
+    /**
+     * @var float|null
+     */
     private $quantity;
+
+    /**
+     * @var int|null
+     */
     private $discountTypeId;
+
+    /**
+     * @var float|null
+     */
     private $discountRate;
+
+    /**
+     * @var float|null
+     */
     private $discountSum;
+
+    /**
+     * @var float|null
+     */
     private $taxRate;
+
+    /**
+     * @var bool|null
+     */
     private $taxIncluded;
+
+    /**
+     * @var bool|null
+     */
     private $customized;
+
+    /**
+     * @var int|null
+     */
     private $measureCode;
+
+    /**
+     * @var string|null
+     */
     private $measureName;
+
+    /**
+     * @var int|null
+     */
     private $sort;
 }
 
